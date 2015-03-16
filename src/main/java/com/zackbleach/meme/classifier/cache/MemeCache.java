b1402f724ba310f508d41ad4597b9c18882afa08
Zack Bleach <zackb@brandwatch.com>
2015-03-16 08:28:44 -0700

    Returns Jedis resource to pool after using

--- src/main/java/com/zackbleach/meme/classifier/cache/MemeCache.java
+++ src/main/java/com/zackbleach/meme/classifier/cache/MemeCache.java
@@ -23,9 +23,7 @@ import com.zackbleach.meme.scraper.ScrapedImage;
 
 @Repository
 public class MemeCache {
-    private static final Log log =
-        LogFactory.getLog(MemeCache.class);
-
+    private static final Log log = LogFactory.getLog(MemeCache.class);
 
     @Autowired
     private RedisTemplate<String, Map<String, String>> redis;
@@ -51,25 +49,34 @@ public class MemeCache {
     }
 
     private Meme getFromCache(String url) throws IOException {
-           Jedis jedis = jedisPool.getResource();
-           String path = (String) jedis.hget(url, PATH);
-           log.info("Getting from path: " + path);
-           String name = (String) jedis.hget(url, NAME);
-           BufferedImage image = imageUtils.readFromDisk(path);
-           return new Meme(name, url, image);
+        Jedis jedis = jedisPool.getResource();
+        try {
+            String path = (String) jedis.hget(url, PATH);
+            log.info("Getting from path: " + path);
+            String name = (String) jedis.hget(url, NAME);
+            BufferedImage image = imageUtils.readFromDisk(path);
+            return new Meme(name, url, image);
+        } finally {
+            jedisPool.returnResource(jedis);
+        }
     }
 
-    //TODO: there's a chance that names will overlap here. Fix.
+    // TODO: there's a chance that names will overlap here. Fix.
     private Meme saveToCache(String url, String name) throws IOException,
             URISyntaxException {
         Jedis jedis = jedisPool.getResource();
-        BufferedImage image = imageUtils.getImageFromUrl(url);
-        String filename = name.replaceAll(" ", "-") + "_" + FilenameUtils.getBaseName(url)
-            + "." + FilenameUtils.getExtension(url);
-        String path = MEME_STORAGE_DIR + filename;
-        imageUtils.saveToDisk(path, image);
-        jedis.hset(url, PATH, path);
-        jedis.hset(url, NAME, name);
-        return new Meme(name, url, image);
+        try {
+            BufferedImage image = imageUtils.getImageFromUrl(url);
+            String filename = name.replaceAll(" ", "-") + "_"
+                    + FilenameUtils.getBaseName(url) + "."
+                    + FilenameUtils.getExtension(url);
+            String path = MEME_STORAGE_DIR + filename;
+            imageUtils.saveToDisk(path, image);
+            jedis.hset(url, PATH, path);
+            jedis.hset(url, NAME, name);
+            return new Meme(name, url, image);
+        } finally {
+            jedisPool.returnResource(jedis);
+        }
     }
 }
